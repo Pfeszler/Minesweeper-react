7f8c62af5b1734b8bfe76a50555e424cf2a12959
Pfeszler <pfeszler@gmail.com>
2020-10-29 11:47:11 +0100

    added setDimensions function to starting button

--- src/StartingScreen/index.js
+++ src/StartingScreen/index.js
@@ -1,15 +1,16 @@
 import React from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { selectStarted, startGame } from "../gameSlice"
+import { selectStarted, setDimensions, startGame } from "../gameSlice"
 
 
 const StartingScreen = () => {
 
-    const started = useSelector(selectStarted)
-    const dispatch = useDispatch()
+    const started = useSelector(selectStarted);
+    const dispatch = useDispatch();
 
     const onStartClick = () => {
-        dispatch(startGame())
+        dispatch(startGame());
+        dispatch(setDimensions({ height: 8, width: 8 }));
     }
 
     return (
--- src/gameSlice.js
+++ src/gameSlice.js
@@ -5,22 +5,34 @@ const gameSlice = createSlice({
     name: "game",
     initialState: {
         started: false,
+        dimensions: {
+            width: 0,
+            height: 0,
+            area: 0,
+        },
         fields: [],
     },
     reducers: {
         startGame: state => {
             state.started = true;
-        }
+        },
+        setDimensions: (state, { payload }) => {
+            state.dimensions.height = payload.height;
+            state.dimensions.width = payload.width;
+            state.dimensions.area = payload.width * payload.height
+        },
 
     }
 
 });
 
 export const {
-    startGame
+    startGame,
+    setDimensions,
 } = gameSlice.actions;
 
 export const selectGame = state => state.game;
 export const selectStarted = state => selectGame(state).started
+export const selectDimensions = state => selectGame(state).dimensions
 export const selectFields = state => selectGame(state).fields
 export default gameSlice.reducer
